e2960c0e5b800801e5e9420d515cf4316cb97826
Diego de Melo Gonzaga <diego-gonzaga@live.com>
2020-08-10 17:11:23 -0300

    Changing python minimun to 3.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as requirements:
 
 setuptools.setup(
     name="fluxo-aws",
-    version="0.0.1",
+    version="0.0.2",
     author="Brenno Flavio",
     author_email="brenno.flavio@fluxoresultados.com.br",
     description="Provides a bunch of functions and utils to help you build applications on top of Amazon Web Services Resources",
@@ -21,6 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=install_requires,
 )
